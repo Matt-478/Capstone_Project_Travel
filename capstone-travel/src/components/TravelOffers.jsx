1bfb8187eb7da4a3201219b72cf7658b7e8bc3cb
Matīss Komļevs <matisskomlevs@gmail.com>
2021-11-14 10:27:45 +0200

    Created new component for displaying travelling

---
+++ capstone-travel/src/components/TravelOffers.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+
+
+const TravelOffers = () => {
+  return(
+    <>
+      <p>Here we'll display all of our info about flights to cool&great places</p>
+    </>
+  )
+}
+
+// AMADEUS
+//  bearer   Qib3QfOzZG1a6g8r8zX0Kx9XhtA8XBS6
+//  secret   BzObmAeMp1ClNDtn
