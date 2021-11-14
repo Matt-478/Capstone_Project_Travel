f5bf01a126ad3d23451ae0d20cae5969be9d08d5
Matīss Komļevs <matisskomlevs@gmail.com>
2021-11-14 20:20:02 +0200

    received data from Amadeus api

--- capstone-travel/src/App.js
+++ capstone-travel/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import TravelOffers from './components/TravelOffers'
@@ -6,13 +7,38 @@ import HomePage from './components/HomePage'
 
 function App() {
 
+  const[query, setQuery] = useState("")
+  const[realState, setRealState] = useState("")
+
+  const handleChange = (e) => {
+    setQuery(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setRealState(query)
+  }
 
   return (
     <div className="App">
       <Router>
-        
-        <Route path="/home" exact component={HomePage} />
-        <Route path="/flight" exact component={TravelOffers}/>
+        <div className="wrapper" >
+          <form onSubmit={handleSubmit}>
+          <div className="searchBar" >
+            <input id="searchQueryInput" type="text" name="searchQueryInput" placeholder="Search" value={query} onChange={handleChange}/>
+            <button id="searchQuerySubmit" type="submit" name="searchQuerySubmit">
+              <svg  viewBox="0 0 24 24">
+              <path fill="#666666" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+              </svg>
+            </button>
+          </div>
+          </form>
+        </div>
+
+
+        <Route path="/home" exact render={(props) => <HomePage realState={realState} query={query} {...props}/>} ></Route>
+        <Route path="/flight" exact render={(props) => <TravelOffers realState={realState} {...props}/>}/>
+    {/* <Route path="/" exact render={(props) => <Home title="Strive" {...props} />} /> */}
       </Router>
     </div>
   );
--- capstone-travel/src/components/TravelOffers.jsx
+++ capstone-travel/src/components/TravelOffers.jsx
@@ -1,10 +1,33 @@
 import { useState, useEffect } from 'react'
 
 
-const TravelOffers = () => {
+const TravelOffers = ({ realState }) => {
+
+  const[flightInfo, setFlightInfo] = useState([])
+
+  useEffect(() => {
+    fetchFlights(realState)
+  },[realState])
+
+  
+  const fetchFlights = async(query) => {
+    const response = await fetch('https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=PAR&maxPrice=200', {
+      headers: {
+          'Authorization': 'Bearer FRWl20u61KT9sivvzfk4j7jv5v4Y'
+        }
+      });
+    const data = await response.json()
+    setFlightInfo(data)
+    console.log(flightInfo)
+
+    // checking if the city typed is the same as in the query
+    // console.log(query)
+  }
+
+
   return(
     <>
-      <p>Here we'll display all of our info about flights to cool&great places</p>
+      <p>Here we'll display all of our info about flights to cool and great places</p>
     </>
   )
 }
@@ -14,3 +37,6 @@ export default TravelOffers
 // AMADEUS
 //  bearer   Qib3QfOzZG1a6g8r8zX0Kx9XhtA8XBS6
 //  secret   BzObmAeMp1ClNDtn
+
+// id Qib3QfOzZG1a6g8r8zX0Kx9XhtA8XBS6
+// token FRWl20u61KT9sivvzfk4j7jv5v4Y
