7ac1cb0872e8b4219f8229f46435d8399947a54c
Matīss Komļevs <matisskomlevs@gmail.com>
2022-05-08 16:02:49 +0300

    removed comments

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -13,17 +13,3 @@ const VideoCard = ({id, src, query}) => {
 }
 
 export default VideoCard
-
-
-    // console.log("items passed down in Video Card", items),
-
-    // <iframe src={src}
-    //     frameborder='0'
-    //     allowfullscreen='false'
-    //     allow='autoplay; encrypted-media'
-    //     title='video'
-    //     width='500px'
-    //     height='auto'
-    //     frameBorder="0"
-    //     className='video-responsive'
-    //   />
